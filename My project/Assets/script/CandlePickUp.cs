f2ace30f967d2012445da527a54acea246ea840c
LandonSims23 <landon.sims@hgs.hiddengeniusproject.org>
2025-08-01 11:47:38 -0700

    zombie game final update on candle

---
+++ My project/Assets/script/CandlePickUp.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+
+public class CandlePickUp : MonoBehaviour
+{
+    [SerializeField] bool canPick;
+
+    [SerializeField] GameObject textOnScreen;
+    [SerializeField] GameObject tableCandle;
+    [SerializeField] GameObject handCandle;
+    
+
+
+    private void Update()
+    {
+        if (canPick == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                this.GetComponent<BoxCollider>().enabled = false;
+                tableCandle.SetActive(false);
+                handCandle.SetActive(false);
+            }
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (PlayerCasting.distancefromtarget < 5)
+        {
+            canPick = true;
+            UIController.actionText = "Candle";
+            UIController.commandText = "Pick Up";
+            UIController.uiActive = true;
+        }
+        else
+        {
+            canPick = false;
+            UIController.actionText = "";
+            UIController.commandText = "";
+            UIController.uiActive = false;
+        }
+    }
+
+
+    void OnMouseExit()
+    {
+        canPick = false;
+        UIController.actionText = "";
+        UIController.commandText = "";
+        UIController.uiActive = false;
+    }
+    
+}
